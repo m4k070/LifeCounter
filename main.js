7eda2a1b67067f8a826a1a48b23441b5a745e59a
Makoto Ito <ito.makoto@interlink-j.com>
2018-05-14 13:44:09 +0900

    scss追加

--- main.js
+++ main.js
@@ -2,79 +2,69 @@
  * 2P側のライフ表示を上下逆表示にするかのフラグ
  */
 var isReverse2P = false;
-
 /**
  * ライフを増やす
  */
 function inc(event) {
-    var elms = event.target.parentNode.getElementsByClassName("life");
+    var elms = event.target.parentNode.getElementsByClassName("life-inner");
     var e = elms[0];
     var str = e.innerText;
     var life = parseInt(str);
     e.innerText = ++life;
 }
-
 /**
  * ライフを減らす
  */
 function dec(event) {
-    var elms = event.target.parentNode.getElementsByClassName("life");
+    var elms = event.target.parentNode.getElementsByClassName("life-inner");
     var e = elms[0];
     var str = e.innerText;
     var life = parseInt(str);
     life = Math.max(0, life - 1);
     e.innerText = life;
 }
-
 /**
  *
  */
 function addClickLostEvent(cls) {
     var elms = document.getElementsByClassName(cls);
-
     for (var i = 0; i < elms.length; i++) {
         var elm = elms[i];
         elm.addEventListener('click', dec);
     }
 }
-
 /**
  *
  */
 function addClickGainEvent(cls) {
     var elms = document.getElementsByClassName(cls);
-
     for (var i = 0; i < elms.length; i++) {
         var elm = elms[i];
         elm.addEventListener('click', inc);
     }
 }
-
 /**
  *
  */
 function addResetEvent(id) {
     var elm = document.getElementById(id);
-
-    elm.addEventListener('click', function(event) {
-        var elms = document.getElementsByClassName("life");
+    elm.addEventListener('click', function (event) {
+        var elms = document.getElementsByClassName("life-inner");
         for (var i = 0; i < elms.length; i++) {
             var life = elms[i];
             life.innerText = 20;
         }
     });
 }
-
 /**
  *
  */
 function addReverseEvent(id) {
     var elm = document.getElementById(id);
-
-    elm.addEventListener('click', function(event) {
+    elm.addEventListener('click', function (event) {
         isReverse2P = !isReverse2P;
         // rotate変更
-        var e = document.getElementById("player2-life");
+        var e = document.getElementById("player2-area");
         if (isReverse2P) {
             e.style.MozTransform = "rotate(180deg)";
             e.style.webkitTransform = "rotate(180deg)";
@@ -85,34 +75,40 @@ function addReverseEvent(id) {
         }
     });
 }
-
 /**
  *
  */
 function onOrientationChange() {
     var root = document.getElementById("root");
     var toolbar = document.getElementById("toolbar");
+    var life1p = document.getElementById("player1-life");
+    var life2p = document.getElementById("player2-life");
+    var life1pIn = document.getElementById("life-1p");
+    var life2pIn = document.getElementById("life-2p");
 
     if (Math.abs(window.orientation) === 90) {
-        root.style.flexDirection = "row";
-        toolbar.style.height = "100%";
-        toolbar.style.width = "4vmin";
-        toolbar.style.flexDirection = "column";
+        root.className = "root-h";
+        toolbar.className ="toolbar-h";
+        life1p.className = "life-h";
+        life2p.className = "life-h";
+        life1pIn.className = "life-inner life-inner-h";
+        life2pIn.className = "life-inner life-inner-h";
     }
     else {
-        root.style.flexDirection = "column";
-        toolbar.style.height = "4vmin";
-        toolbar.style.width = "100%";
-        toolbar.style.flexDirection = "row";
+        root.className = "root-v";
+        toolbar.className ="toolbar-v";
+        life1p.className = "life-v";
+        life2p.className = "life-v";
+        life1pIn.className = "life-inner life-inner-v";
+        life2pIn.className = "life-inner life-inner-v";
     }
 }
 
-window.onload = function() {
+window.onload = function () {
     addClickGainEvent("btn-north");
     addClickLostEvent("btn-south");
     addResetEvent("reset-btn");
     addReverseEvent("reverse-btn");
-
     window.addEventListener("orientationchange", onOrientationChange);
     onOrientationChange();
-}
+};
